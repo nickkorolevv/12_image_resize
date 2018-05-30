1e0637abeaa09077412712915b2aba6ed354d53a
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-30 12:18:58 +0300

    split complicated function into
    several small functions

--- image_resize.py
+++ image_resize.py
@@ -14,71 +14,89 @@ def create_parser():
     return parser
 
 
-def get_sizes(input_image_path, width, height, scale):
+def open_image(input_image_path):
     original_image = Image.open(input_image_path)
-    width_original, height_original = original_image.size
+    return original_image.size
+
+
+def get_ratio(new_size, original_size):
+    ratio = (new_size / float(original_size))
+    return ratio
+
+
+def is_ratio_changed(width_original, height_orignal, width, height):
+    permissible_error = 0.005
+    proportion_change = width / width_original - height / height_orignal
+    return abs(proportion_change) > permissible_error
+
+
+def get_sizes(scale, original_sizes, width=None, height=None):
+    width_original, height_original = original_sizes
     if (width and height):
         max_size = (width, height)
-        logging.warning("Изображение может быть непропорциональным")
+        if is_ratio_changed(width_original, height_original, width, height):
+            logging.warning("Изображение может быть непропорциональным")
         return max_size
     elif width:
-        max_size = (width, height_original)
+        max_size = (
+            width,
+            int((get_ratio(width, width_original))*height_original)
+        )
         return max_size
     elif height:
-        max_size = (width_original, height)
+        max_size = (
+            int((get_ratio(height, height_original))*width_original),
+            height
+        )
         return max_size
     elif scale:
         max_size = (int(scale*width_original), int(scale*height_original))
         return max_size
 
 
-def scale_by_resize(output_image_path, max_size):
-    original_image = Image.open(input_image_path)
-    resized_image = original_image.resize(max_size, Image.ANTIALIAS)
-    out_image_path = "{}{}x{}.jpg".format(
-        output_image_path,
-        max_size[0],
-        max_size[1]
-    )
-    resized_image.save(out_image_path)
-
-
-def scale_by_thumbnail(output_image_path, max_size):
-    original_image = Image.open(input_image_path)
-    original_image.thumbnail(max_size, Image.ANTIALIAS)
-    out_image_path = "{}{}x{}.jpg".format(
-        output_image_path,
-        max_size[0],
-        max_size[1]
-    )
-    original_image.save(out_image_path)
-
-
-if __name__ == "__main__":
-    parser = create_parser()
-    namespace = parser.parse_args()
-    scale, input_image_path, width, height = (
-        namespace.scale,
-        namespace.input,
-        namespace.width,
-        namespace.height
-    )
-    if namespace.output is None:
+def get_output_path():
+    if parsargs.output:
+        output_dir = parsargs.output
+        output_image_path = os.path.join(
+            output_dir,
+            os.path.splitext(input_image_path)[0]
+        )
+    elif parsargs.output is None:
         output_dir = os.getcwd()
         output_image_path = os.path.join(
             output_dir,
             os.path.splitext(input_image_path)[0]
         )
-    else:
-        output_dir = namespace.output
-        output_image_path = os.path.join(
-            output_dir,
-            os.path.splitext(input_image_path)[0]
-        )
-    if not(os.path.isdir(output_dir)):
-        exit("Директории не существует")
-    max_size = get_sizes(input_image_path, width, height, scale)
-    if (width and height) or scale:
-        scale_by_resize(output_image_path, max_size)
-    elif (width or height):
-        scale_by_thumbnail(output_image_path, max_size)
+    out_image = "{}__{}x{}.jpg".format(
+        output_image_path,
+        max_size[0],
+        max_size[1]
+    )
+    return out_image
+
+
+def scale_by_resize(output_image_path, max_size):
+    original_image = Image.open(input_image_path)
+    resized_image = original_image.resize(max_size, Image.ANTIALIAS)
+    resized_image.save(output_image_path)
+
+
+if __name__ == "__main__":
+    parser = create_parser()
+    parsargs = parser.parse_args()
+    scale, input_image_path, width, height = (
+        parsargs.scale,
+        parsargs.input,
+        parsargs.width,
+        parsargs.height
+    )
+
+    if not(os.path.exists(parsargs.input)):
+        exit("Файла не сущетсвует")
+    if scale and (width or height):
+        exit("Ошибка!")
+    original_sizes = open_image(input_image_path)
+    max_size = get_sizes(scale, original_sizes, width, height)
+    output_image_path = get_output_path()
+    scale_by_resize(output_image_path, max_size)
+    print("Готово!")
